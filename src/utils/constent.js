1c8f55e0f052c5603ff02e0313f939f82f2665ca
Ratan Singh Yadav <108169130+RatanSinghYadav@users.noreply.github.com>
2024-09-26 17:34:05 +0530

    Update constent.js

--- src/utils/constent.js
+++ src/utils/constent.js
@@ -1,4 +1,5 @@
 module.exports = Object.freeze({
     // url: "http://localhost:8000",
-    url:`brlyapps-btdmbcajh3hfcwdb.centralindia-01.azurewebsites.net`
+    url: `https://vendor-registration-api.onrender.com`
+    // url:`brlyapps-btdmbcajh3hfcwdb.centralindia-01.azurewebsites.net`
 })
