e3a3eb89eb6744cef70f24ea8b1c280a5d8ac202
RatanSinghYadav <ratan.120375@gmail.com>
2024-10-11 18:14:20 +0530

    bug_fix

--- src/utils/constent.js
+++ src/utils/constent.js
@@ -1,6 +1,4 @@
 module.exports = Object.freeze({
     // url: "http://localhost:8000",
     url:`https://brlyapps-btdmbcajh3hfcwdb.centralindia-01.azurewebsites.net`
-    // url: "http://localhost:8000",
-    url:`https://brlyapps-btdmbcajh3hfcwdb.centralindia-01.azurewebsites.net`
 })
