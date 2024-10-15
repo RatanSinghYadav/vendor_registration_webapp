f0d4553ae6046c839560870714e7e2041f826705
RatanSinghYadav <ratan.120375@gmail.com>
2024-10-15 12:08:47 +0530

    show_document_in_edit

--- src/components/vendor/vendor edit/editVendor.js
+++ src/components/vendor/vendor edit/editVendor.js
@@ -373,7 +373,7 @@ const EditVendor = () => {
                         </Row>
 
                         {/* Downloaded files */}
-                        {/* <Divider orientation='left' orientationMargin={0} style={{ color: '#334155' }}>Files</Divider>
+                        <Divider orientation='left' orientationMargin={0} style={{ color: '#334155' }}>Files</Divider>
                         <Row gutter={[16, 16]}>
                             <Col span={6}>
                                 <h6>Incorporation Certificate</h6>
@@ -411,7 +411,7 @@ const EditVendor = () => {
                                     Open
                                 </Button>
                             </Col>
-                        </Row> */}
+                        </Row>
                     </Card>
 
                     {/* For Purchase  */}
