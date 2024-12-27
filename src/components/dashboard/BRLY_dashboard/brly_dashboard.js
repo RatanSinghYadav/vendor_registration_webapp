e51eb65536db2012da6f23151b24ff9bec4cf37f
RatanSinghYadav <ratan.120375@gmail.com>
2024-12-27 16:50:35 +0530

    bug_fix

--- src/components/dashboard/BRLY_dashboard/brly_dashboard.js
+++ src/components/dashboard/BRLY_dashboard/brly_dashboard.js
@@ -5,6 +5,7 @@ import SendInvite from '../Admin_dashboard/sendInvite';
 import { SearchOutlined } from '@ant-design/icons';
 import '../../../App.css';
 import { useNavigate, useParams } from 'react-router-dom';
+import Highlighter from 'react-highlight-words';
 import Status from '../Admin_dashboard/status.js';
 import { FileTextOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import ExportInExcel from './exportInExcel.js';
