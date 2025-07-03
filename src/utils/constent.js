904f3a91575e35b5897948d064ddd77af9512f6d
Ratan Singh Yadav <108169130+RatanSinghYadav@users.noreply.github.com>
2025-07-03 15:54:35 +0530

    Update constent.js

--- src/utils/constent.js
+++ src/utils/constent.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
     // url: "http://localhost:8000",
-    // url: `https://vendify-server-b4b8aabeh5ajczfx.centralindia-01.azurewebsites.net`
-    url:`https://brlyapps-btdmbcajh3hfcwdb.centralindia-01.azurewebsites.net`
+    url: `https://vendify-server-b4b8aabeh5ajczfx.centralindia-01.azurewebsites.net`
+    // url:`https://brlyapps-btdmbcajh3hfcwdb.centralindia-01.azurewebsites.net`
 })
